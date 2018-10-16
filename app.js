65498eb1273b076e947e10a7a94f13e27575917e
likesatie@gmail.com <likesatie@gmail.com>
2018-10-16 09:06:47 +0300

    Clear the task-list button functionality is done, filter tasks functionality is done

--- app.js
+++ app.js
@@ -8,11 +8,14 @@ loadEventListeners();
 
 function loadEventListeners() {
   form.addEventListener('submit', addTask);
+  taskList.addEventListener('click', removeTask);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e) {
   if (taskInput.value === '') {
-    alert('Add a task');
+    alert('Введите текст');
   }
 
   const li = document.createElement('li');
@@ -28,4 +31,32 @@ function addTask(e) {
   taskInput.value = '';
 
   e.preventDefault();
+}
+
+function removeTask(e) {
+  if (e.target.parentElement.classList.contains('delete-item')) {
+    if (confirm('Вы уверены?')) {
+      e.target.parentElement.parentElement.remove();
+    }
+  }
+}
+
+function clearTasks() {
+  // taskList.innerHTML = '';
+  while (taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e) {
+  const text = e.target.value.toLowerCase();
+
+  document.querySelectorAll('.collection-item').forEach(function(task) {
+    const item = task.firstChild.textContent;
+    if (item.toLowerCase().indexOf(text) != -1) {
+      task.style.display = 'block';
+    } else {
+      task.style.display = 'none';
+    }
+  });
 }
